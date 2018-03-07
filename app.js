6e609a7531209cd22c989a4538bc6ff2cf37ac02
Nassi Harel <nassi9@gmail.com>
2018-03-07 16:04:29 +0200

    updates

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const uuidv4 = require('uuid/v4');
+const vm = require('vm');
 const Etcd = require('@hkube/etcd');
 const { Consumer } = require('@hkube/producer-consumer');
 
@@ -29,58 +30,17 @@ let pipelineName = null;
 const useRandomTimeout = false;
 const useFixedTimeout = true;
 const randomBatchFail = false;
-const fixedTimeout = 1000;
+const fixedTimeout = 10000;
 let currentJob = null;
 
 const etcd = new Etcd();
-async function init() {
+
+(async function init() {
     etcd.init({ etcd: etcdOptions, serviceName });
     await etcd.discovery.register({ serviceName });
-
-    setInterval(() => {
-        updateDiscovery({ status: 'active' });
-    }, 2000)
-
+    await updateDiscovery({ status: 'active' });
     start();
-}
-
-function start() {
-    etcd.jobs.on('change', (res) => {
-        console.log(`job stopped ${jobId}. result: ${JSON.stringify(res)}`);
-        etcd.jobs.unwatch({ jobId });
-        currentJob.done();
-    });
-
-    const consumer = new Consumer(setting);
-    consumer.on('job', (job) => {
-
-        jobId = job.data.jobID;
-        taskId = job.data.taskID;
-        pipelineName = job.data.pipeline_name;
-
-        updateDiscovery({ status: 'active', ...job.data });
-
-        etcd.jobs.watch({ jobId });
-        currentJob = job;
-        console.log(`job arrived for node ${job.data.nodeName} with input: ${JSON.stringify(job.data.input)}`);
-        etcd.tasks.setState({ jobId, taskId, status: 'active' });
-
-        if (useRandomTimeout || useFixedTimeout) {
-            const timeout = useFixedTimeout ? fixedTimeout : random() * 1000;
-            console.log(`starting timeout of ${timeout / 1000} sec`);
-
-            setTimeout(() => {
-                endJob(job);
-            }, timeout);
-        }
-        else {
-            endJob(job);
-        }
-    });
-
-    consumer.register(setting);
-    console.log(`worker ready for algo ${setting.job.type}`);
-}
+})();
 
 async function updateDiscovery(data) {
     return etcd.discovery.updateRegisteredData({
@@ -92,23 +52,75 @@ async function updateDiscovery(data) {
     });
 }
 
-async function endJob(job) {
-    const isBatch = job.data.batchIndex >= 0;
-    const rand = isBatch && randomBatchFail ? random() : 20;
-    const input = job.data.input[0];
+function start() {
+    etcd.jobs.on('change', (res) => {
+        console.log(`job stopped ${jobId}. result: ${JSON.stringify(res)}`);
+        etcd.jobs.unwatch({ jobId });
+        currentJob.done();
+    });
 
-    if (input > 10) {
-        const error = new Error('some strange error');
-        await etcd.tasks.setState({ jobId, taskId, error: error.message, status: 'failed' });
-        await etcd.jobs.unwatch({ jobId });
-        job.done(error);
-        console.log(`job ${job.id} failed with ${error.message}`);
+    const consumer = new Consumer(setting);
+    consumer.on('job', async (job) => {
+        console.log(`job arrived for node ${job.data.nodeName} with input: ${JSON.stringify(job.data.input)}`);
+        currentJob = job;
+        jobId = job.data.jobID;
+        taskId = job.data.taskID;
+        pipelineName = job.data.pipelineName;
+        await etcd.tasks.setState({ jobId, taskId, status: 'active' });
+        const input = job.data.input[0];
+        const code = job.data.info && job.data.info.extraData && job.data.info.extraData.code && job.data.extraData.code.join('\n');
+
+        if (code) {
+            const wrapper = (input) => {
+                return new Promise((resolve, reject) => {
+                    const userFunctionPromise = Promise.resolve(vm.runInThisContext(`(${code})`)(input));
+                    userFunctionPromise.then((result) => {
+                        return resolve(result);
+                    }).catch(error => {
+                        return reject(error);
+                    })
+                });
+            }
+            const output = await wrapper(job.data.input);
+            endJob(output);
+        }
+        else {
+            endJob(input);
+        }
+    });
+
+    consumer.register(setting);
+    console.log(`worker ready for algo ${setting.job.type}`);
+}
+
+async function endJob(output) {
+    if (useRandomTimeout || useFixedTimeout) {
+        const timeout = useFixedTimeout ? fixedTimeout : random() * 1000;
+        console.log(`starting timeout of ${timeout / 1000} sec`);
+
+        setTimeout(() => {
+            _endJob(output);
+        }, timeout);
     }
     else {
-        await etcd.tasks.setState({ jobId, taskId, result: input, status: 'succeed' });
-        await etcd.jobs.unwatch({ jobId });
-        job.done();
-        console.log(`job ${job.id} succeed with ${input}`);
+        _endJob(output);
+    }
+}
+
+async function _endJob(output) {
+    const isBatch = currentJob.data.batchIndex >= 0;
+    const rand = isBatch && randomBatchFail ? random() : 20;
+
+    if (rand < 20) {
+        const error = new Error('some strange error');
+        await etcd.tasks.setState({ jobId, taskId, error: error.message, status: 'failed' });
+        currentJob.done(error);
+        console.log(`job ${currentJob.id} failed with ${error.message}`);
+    }
+    else {
+        await etcd.tasks.setState({ jobId, taskId, result: output, status: 'succeed' });
+        currentJob.done();
+        console.log(`job ${currentJob.id} succeed with ${output}`);
     }
 }
 
@@ -116,11 +128,7 @@ function random() {
     return Math.floor(Math.random() * 10) + 1
 }
 
-init();
-
-_handleErrors();
-
-function _handleErrors() {
+(function _handleErrors() {
     process.on('exit', (code) => {
         console.log('exit' + (code ? ' code ' + code : ''));
     });
@@ -140,7 +148,7 @@ function _handleErrors() {
         console.log('uncaughtException: ' + error.message);
         process.exit(1);
     });
-}
+})();
 
 
 
